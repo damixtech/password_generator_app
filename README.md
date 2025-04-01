bcfcbe756c1cb36d6400dd1278204dd82b5554f9
Damian AR <damitech@tutamail.com>
2025-04-01 12:17:53 +0200

    Changing some styles and adding readme file

---
+++ README.md
@@ -0,0 +1,128 @@
+
+# 🇬🇧 - PASSWORD GENERATOR 🎙️
+
+This project is a desktop application created with **Python** and **Tkinter** that generates **secure and customizable** passwords. It allows the user to choose the length of the password and select which types of characters to include, such as **uppercase** letters, **lowercase** letters, **numbers**, and **symbols**. Compatible with **Mac**, **Linux**, and **Windows**.
+
+
+![screenshot](./assets/app_interface.png)
+
+
+## 🚀 Why is this project useful?
+
+This password generator is useful because it helps you create **strong and unique passwords** to protect your online accounts. Customizing the password allows you to tailor it to your specific needs, ensuring both **security** and **usability**. Additionally, it prevents the creation of weak or repetitive passwords.
+
+---
+
+## 🛠️ Requirements
+
+Make sure you have the following requirements to run the application:
+
+- **Python 3.x** installed. You can get it [here](https://www.python.org/downloads/).
+- **Tkinter** (for the graphical interface). It is usually included with Python, but if you don't have it, you can install it with:
+  ```bash
+  sudo apt-get install python3-tk  # For Linux
+  brew install python-tk  # For Mac
+## 💻 How to start
+
+- First, clone the repository.
+    ```bash
+  git clone https://github.com/damixtech/text_to_voice_app.git
+<!-- - Then, install the dependencies.
+  ```bash
+  pip install -r requeriments.txt    -->
+
+## 💾 How to run
+- Running the code:
+    - Run the code from your code editor (e.g., VSCode).
+  
+<!-- - Running the executable file (Dist folder)
+    - Run the application from the executable file in the "dist" folder. It is compatible with operating systems such as **Linux**, **Mac**, and **Windows**. -->
+
+## ✒️ How to use
+
+- **Run the application**: Open the desktop application on your system.
+
+- **Set the parameters**:
+
+    - **Password length**: Adjust the slider to select the desired password length.
+
+    - **Character types**: Check the options to include uppercase letters, lowercase letters, numbers, and/or symbols according to your preferences.
+
+- **Generate the password**: Click the button to generate a new secure password with the selected settings.
+
+- **Copy the password**: The generated password will appear in the interface, and you can easily copy it for use in your accounts.
+
+## 🤝 Contributing
+This repository is completely open to contributions and improvements.
+
+1. Fork the repository.
+2. Create a new branch for your contribution.
+3. Make your changes and commit them.
+4. Push your changes to GitHub.
+5. Open a pull request on GitHub.
+   
+### I will be happy to see your contributions.
+---
+# 🇪🇸 - PASSWORD GENERATOR 🎙️
+
+Este proyecto es una aplicación de escritorio creada con **Python** y **Tkinter** que genera contraseñas seguras y personalizadas. Permite al usuario elegir la longitud de la contraseña y seleccionar qué tipos de caracteres incluir, como mayúsculas, minúsculas, números y símbolos.Compatible con **Mac**, **Linux** y **Windows**.
+
+![screenshot](./assets/app_interface.png)
+
+
+## 🚀 ¿Por qué es útil este proyecto?
+
+Este generador de contraseñas es útil porque te ayuda a crear contraseñas **fuertes y únicas** para proteger tus cuentas en línea. La personalización de la contraseña te permite ajustarla a tus **necesidades específicas**, garantizando tanto **seguridad** como **facilidad de uso**. Además, evita la creación de contraseñas débiles o repetitivas.
+
+---
+
+## 🛠️ Requisitos
+
+Asegúrate de tener los siguientes requisitos para ejecutar la aplicación:
+
+- **Python 3.x** instalado. Puedes obtenerlo [aquí](https://www.python.org/downloads/).
+- **Tkinter** (para la interfaz gráfica). Generalmente se incluye con Python, pero si no lo tienes, puedes instalarlo con:
+  ```bash
+  sudo apt-get install python3-tk  # Para Linux
+  brew install python-tk  # Para Mac
+
+## 💻 Cómo empezar
+
+- Lo primero que tendrás que hacer es **clonar** el repositorio.
+    ```bash
+  git clone https://github.com/damixtech/text_to_voice_app.git
+<!-- - Después instala las dependencias.
+  
+    ```bash
+  pip install -r requeriments.txt    -->
+
+## 💾 Cómo ejecutar
+- Ejecutando el código:
+    - Ejecuta el código desde tu editor de código (Por ejemplo VSCode)
+  
+<!-- - Ejecutando el archivo ejecutable (Carpeta Dist)
+    - Ejecuta la aplicación desde el archivo ejecutable de la carpeta **"dist"**. Es compatible con sistemas operativos como **Linux**, **Mac** y **Windows**. -->
+  
+
+## ✒️ Cómo usar
+- **Ejecuta la aplicación**: Abre la aplicación de escritorio en tu sistema.
+
+- **Configura los parámetros**:
+
+  - **Longitud de la contraseña**: Ajusta el deslizador para seleccionar la longitud deseada de la contraseña.
+
+  - **Tipos de caracteres**: Marca las opciones para incluir mayúsculas, minúsculas, números y/o símbolos, según tus preferencias.
+
+- **Genera la contraseña**: Haz clic en el botón para generar una nueva contraseña segura con las configuraciones seleccionadas.
+
+- **Copia la contraseña**: La contraseña generada aparecerá en la interfaz, y podrás copiarla fácilmente para usarla en tus cuentas.
+
+## 🤝 Abierto a contribuciones
+Repositorio totalmente abierto a contribuciones y mejoras.
+1. Haz un **fork** del repositorio
+2. Crea una **rama nueva** para tu contribución
+3. Realiza los cambios y hac el **commit**
+4. Sube tus cambios a **GitHub**
+5. Abre una **pull request** en GitHub
+   
+#### ❤️ Estaré encantado de ver tus aportaciones.
--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from tkinter import *
 import random as rd
 import string
+from ttkbootstrap import Style
 
 
 #Class
 class App():
     def __init__(self, window):
         #Main window
+        #self.style = Style(theme="morph")
         self.window = window
         self.window.title('Password generator')
         self.window.geometry('600x500')
@@ -21,7 +23,7 @@ class App():
         #Method that creates widgets
         self.create_widgets()
 
-    
+
     def create_widgets(self):
         '''Create all the components of the app'''
         ###FRAME: Header
@@ -32,37 +34,40 @@ class App():
                             bg='#4A90E2', fg='#FFFFFF')
         self.header.pack()
         #Subtitle
-        self.subheader = Label(self.header_frame, text='Generate a strong password in seconds', font=('Poppins', 12),
+        self.subheader = Label(self.header_frame, text='Generate a strong password in few seconds', font=('Poppins', 12),
                                bg='#4A90E2', fg='#FFFFFF')
         self.subheader.pack()
         ###FRAME: Password box
-        self.password_frame = LabelFrame(self.window, text="Your password", relief="flat", bg='#4A90E2', fg='#FFFFFF')
-        self.password_frame.config(highlightbackground='white', highlightcolor='white', highlightthickness=2)
-        self.password_frame.pack()
+        self.password_frame = LabelFrame(self.window, text="Your password", width=100, height=50, relief="flat", bg='#4A90E2', fg='#FFFFFF')
+        #White border config
+        self.password_frame.config(highlightbackground='white', highlightcolor='white', highlightthickness=2,
+                                   font=('Poppins',12))
+        self.password_frame.pack(padx=0)
         #Final password
-        self.final_password = Label(self.password_frame, text="", width=29, bg='#4A90E2')
-        self.final_password.pack(side="left")
+        self.final_password = Label(self.password_frame, text="", width=37, bg='#4A90E2')
+        self.final_password.pack(side="left", anchor="w")
         self.copy_button = Button(self.password_frame, text='Copy', command=self.copy_password,
                                   bg="#50E3C2", activebackground='#4A90E2', activeforeground='#FFFFFF',
-                                  relief="flat")
-        self.copy_button.pack(side="right", pady=10)
+                                  relief="flat", font=('Poppins',12))
+        self.copy_button.pack(side="right", pady=10, anchor="e", padx=0)
         ###FRAME: Customize box
-        self.customize_frame = Frame(self.window, width=50, padx=20, pady=20, bg='#4A90E2')
+        self.customize_frame = Frame(self.window, width=70, padx=40, pady=20, bg='#4A90E2')
+        #White border config
         self.customize_frame.config(highlightbackground='white', highlightthickness=2, highlightcolor='white')
         self.customize_frame.pack(pady=20)
         #Title
         self.customize_title = Label(self.customize_frame, text='Customize your password', font=('Poppins', 12),
                                      bg='#4A90E2', fg='#FFFFFF')
-        self.customize_title.config(pady=10)
+        self.customize_title.config(pady=5)
         self.customize_title.pack()
         #Length selector
-        self.length_frame = Frame(self.customize_frame, bg='#4A90E2')
+        self.length_frame = Frame(self.customize_frame, bg='#4A90E2', pady=20)
         self.length_frame.pack(side='left', padx=20)
-        self.length_label = Label(self.length_frame, text="Password Length", bg='#4A90E2', fg='#FFFFFF')
+        self.length_label = Label(self.length_frame, text="Password Length", bg='#4A90E2', fg='#FFFFFF', font=('Poppins',10))
         self.length_label.pack(anchor="w")
         self.length_control = Scale(self.length_frame, variable=self.length_var, from_=8, to=24, resolution=1,
                                     troughcolor='#FFFFFF', orient=HORIZONTAL, bg='#4A90E2', highlightthickness=0,
-                                    relief="flat")
+                                    relief="flat", sliderrelief="flat", activebackground='#50E3C2')
         self.length_control.pack(anchor="w")
         #Character selector
         #FRAME: Radiobuttons
@@ -73,33 +78,37 @@ class App():
                                      highlightthickness=0,
                                      relief="flat",
                                      bg='#4A90E2', fg='#FFFFFF',
-                                     activebackground='#50E3C2')
+                                     activebackground='#50E3C2',
+                                     font=('Poppins',12), selectcolor='#50E3C2')
         self.uppercase.pack(anchor="w")
         self.lowercase = Checkbutton(self.customize_frame, text='Lowercase',
                                      variable=self.lowercase_var,
                                      highlightthickness=0,
                                      relief="flat",
                                      bg='#4A90E2', fg='#FFFFFF',
-                                     activebackground='#50E3C2')
+                                     activebackground='#50E3C2',
+                                     font=('Poppins',12), selectcolor='#50E3C2')
         self.lowercase.pack(anchor="w")
         self.numbers = Checkbutton(self.customize_frame, text='Numbers',
                                    variable=self.numbers_var,
                                    highlightthickness=0, 
                                    relief="flat",
                                    bg='#4A90E2', fg='#FFFFFF',
-                                   activebackground='#50E3C2')
+                                   activebackground='#50E3C2',
+                                   font=('Poppins',12), selectcolor='#50E3C2')
         self.numbers.pack(anchor="w")
         self.symbols = Checkbutton(self.customize_frame, text='Symbols',
                                    variable=self.symbols_var,
                                    highlightthickness=0, relief="flat",
                                    bg='#4A90E2', fg='#FFFFFF',
-                                   activebackground='#50E3C2')
+                                   activebackground='#50E3C2',
+                                   font=('Poppins',12), selectcolor='#50E3C2')
         self.symbols.pack(anchor="w")
         ###WINDOW
         #Generate button
         self.generate_button = Button(self.window, text='Generate', command=self.check_selector,
                                       bg="#50E3C2", activebackground='#4A90E2', activeforeground='#FFFFFF',
-                                      relief="flat")
+                                      relief="flat", font=('Poppins',12))
         self.generate_button.pack()
         
 
@@ -148,15 +157,17 @@ class App():
         for i in range(0, self.final_length):
             self.char = rd.choice(self.characters)
             self.new_password += self.char
-        
         self.show_password()
         
+
     def show_password(self):
-        self.final_password.config(text=self.new_password, fg='#FFFFFF')
-    
+        self.final_password.config(text=self.new_password, fg='#FFFFFF', padx=0, anchor="w", font=('Poppins',10))
+
+
     def copy_password(self):
         password = self.new_password
         app.window.clipboard_append(password)
+        app.window.update()
 
 
 app = App(Tk())
